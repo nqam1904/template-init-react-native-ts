7777860d87cd63924f34d4fe2f671eb07518718a
TaiTran2512 <trantantai25121998@gmail.com>
2022-02-23 11:21:59 +0700

    config navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
+import MainStack from './src/navigation/index'
 
 const App: React.FC = () => {
 	return (
 		<Provider store={store}>
-			<View>
-				<Text>App</Text>
-			</View>
+			<MainStack />
 		</Provider>
 	)
 }
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,23 +1,60 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { navigationRef } from './navigationRef'
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack'
 
-function HomeScreen() {
+import { navigationRef } from 'navigationRef'
+
+function HomeScreen(props: any) {
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+		<View
+			style={{
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}>
 			<Text>Home Screen</Text>
+			<Button title="Go" onPress={() => props.navigation.navigate('Home1')} />
 		</View>
 	)
 }
+function HomeScreen1() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}>
+			<Text>Home Screen 1</Text>
+		</View>
+	)
+}
+export const screenOptionsNativeStack: any = {
+	headerShown: false,
+	customAnimationOnGesture: true,
+	fullScreenGestureEnabled: true,
+	gestureEnabled: true,
+	animationTypeForReplace: 'push',
+	animation: 'slide_from_right',
+	presentation: 'modal',
+}
+
+export const screenOptionsStack: any = {
+	headerShown: false,
+	presentation: 'modal',
+	gestureEnabled: true,
+	...TransitionPresets.SlideFromRightIOS,
+}
+
 const MainStack: React.FC = () => {
 	const Stack = createNativeStackNavigator()
-
 	return (
 		<NavigationContainer ref={navigationRef}>
-			<Stack.Navigator>
+			<Stack.Navigator screenOptions={screenOptionsNativeStack}>
 				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Home1" component={HomeScreen1} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
