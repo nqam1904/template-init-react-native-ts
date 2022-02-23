63f5a9709e642c848237714eadc5b0dea417dcf0
nqam1904 <nghiemminh1904>
2022-02-23 11:50:19 +0700

    config environment

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
-import MainStack from './src/navigation/index'
-
+import MainStack from './src/navigation'
+import Config from 'react-native-config'
+const { ENVIRONMENT } = Config
+console.log('ENVIRONMENT', ENVIRONMENT)
 const App: React.FC = () => {
 	return (
 		<Provider store={store}>
