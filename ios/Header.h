09c0f97068407ad62faf02dab6bd396aeb63145f
nqam1904 <nghiemminh1904>
2022-02-23 16:32:32 +0700

    config ios

---
+++ ios/Header.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  templateProject
+//
+//  Created by WeJelly on 23/02/2022.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ ios/TemplateProject-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  TemplateProject-Bridging-Header.h
+//  templateProject
+//
+//  Created by WeJelly on 23/02/2022.
+//
+
+#ifndef TemplateProject_Bridging_Header_h
+#define TemplateProject_Bridging_Header_h
+
+
+#endif /* TemplateProject_Bridging_Header_h */
